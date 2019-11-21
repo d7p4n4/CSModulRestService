8231c23f167ebfee30dd02cbfd4dc4ac4b7938a2
d7p4n4 <bercikezs199999999@gmail.com>
2019-11-21 07:32:03 +0100

    Add project files.

---
+++ CSModulRestService/MSSQLLogin.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CSModulRestService
+{
+    public class MSSQLLogin
+    {
+        public string baseName { get; set; }
+        public string userName { get; set; }
+        public string password { get; set; }
+        public string serverName { get; set; }
+    }
+}
